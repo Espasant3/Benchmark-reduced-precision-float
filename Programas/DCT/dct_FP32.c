e1675e24ce1e4535e4d519c10e29a638c4259d71
EloiBarconPineiro <eloi.b.pineiro@gmail.com>
2024-11-07 21:38:30 +0100

    Adaptado DCT para generar datos de manera aleatoria

--- Programas/DCT/dct_FP32.c
+++ Programas/DCT/dct_FP32.c
@@ -11,20 +11,20 @@
 
 
 // Definir el tamaño de la señal
-#define N 8
+#define N_SMALL 8
 
 void dct(float *input, float *output) {
     float alpha;
     float sum;
-    for (int k = 0; k < N; k++) {
+    for (int k = 0; k < N_SMALL; k++) {
         if (k == 0) {
-            alpha = sqrt(1.0 / N);
+            alpha = sqrt(1.0 / N_SMALL);
         } else {
-            alpha = sqrt(2.0 / N);
+            alpha = sqrt(2.0 / N_SMALL);
         }
         sum = 0.0;
-        for (int n = 0; n < N; n++) {
-            sum += input[n] * cos(M_PI * (2.0 * n + 1.0) * k / (2.0 * N));
+        for (int n = 0; n < N_SMALL; n++) {
+            sum += input[n] * cos(M_PI * (2.0 * n + 1.0) * k / (2.0 * N_SMALL));
         }
         output[k] = alpha * sum;
     }
@@ -32,35 +32,40 @@ void dct(float *input, float *output) {
 
 int main(int argc, char *argv[]) {
 
-    /*
+    /**/
     if (argc < 2) {
         printf("Uso: %s <tamaño del vector> [<seed>]\n", argv[0]);
         return EXIT_FAILURE;
     }  
 
     int n = atoi(argv[1]);
-    */
 
-    float *input = (float *)malloc(N * sizeof(float));
-    float *output = (float *)malloc(N * sizeof(float));
+    float *input = (float *)malloc(N_SMALL * sizeof(float));
+    float *output = (float *)malloc(N_SMALL * sizeof(float));
 
-    input[0]=1.0f; input[1]=2.0f; input[2]=3.0f; input[3]=4.0f; input[4]=4.0f; input[5]=3.0f; input[6]=2.0f; input[7]=1.0f;
+    //input[0]=1.0f; input[1]=2.0f; input[2]=3.0f; input[3]=4.0f; input[4]=4.0f; input[5]=3.0f; input[6]=2.0f; input[7]=1.0f;
 
-    /*
+    /**/
     unsigned int seed = (argc > 2) ? atoi(argv[2]) : (unsigned int)time(NULL);
     srand(seed);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < N_SMALL; i++) {
         input[i] = ((float)rand() / (float)(RAND_MAX)) * 10.0;
     }
-    */
+
+    printf("Array input: [ ");
+    for (int i = 0; i < N_SMALL; i++) {
+        printf("%f ", input[i]);
+    }
+    printf("\n");
 
     dct(input, output);
 
-    printf("DCT:\n");
-    for (int i = 0; i < N; i++) {
-        printf("%f\n", output[i]);
+    printf("Resultado DCT: [");
+    for (int i = 0; i < N_SMALL; i++) {
+        printf("%f ", output[i]);
     }
+    printf("]\n");
 
     free(input);
     free(output);
