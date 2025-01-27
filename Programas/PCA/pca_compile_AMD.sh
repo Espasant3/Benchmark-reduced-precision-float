38a2b64c7530d0db995518e7e3933acefa7f2bd2
EloiBarconPineiro <eloi.b.pineiro@gmail.com>
2025-01-27 18:43:50 +0100

    Agregados a PCA scripts de compilacion y ejecucion para todas las arquitecturas

--- Programas/PCA/pca_compile_AMD.sh
+++ Programas/PCA/pca_compile_AMD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### SCRIPT DE COMPILACION PARA ARQUITECTURA INTEL x86
+### SCRIPT DE COMPILACION PARA ARQUITECTURA AMD x86_64
 
 # Inicializar variables
 force_run=false
@@ -43,10 +43,10 @@ script_dir="$(dirname "$0")"
 # Cambiar al directorio del script
 cd "$script_dir"
 
-
 ### COMPILACION DEL PROGRAMA BASE
 
 gcc-14 $COMMON_FLAGS pca_FP32.c -o pca_FP32 $OPT_FLAGS $LINK_FLAGS
+
 
 ### COMPILACION DEL PROGRAMA DE CON FLOAT DE 16 BITS QUE EMPLEA EL TIPO DE DATO _Float16
 
@@ -55,31 +55,34 @@ if grep -q "sse2" /proc/cpuinfo; then
 
     gcc-14 $COMMON_FLAGS pca_FP16.c -o pca_FP16 -fexcess-precision=16 $OPT_FLAGS $LINK_FLAGS
 
-    if grep -q "avx512fp16" /proc/cpuinfo; then
-        
-        echo "AVX512FP16 support detected. Compiling with -mavx512fp16."
-        COMMON_FLAGS+=" -mavx512fp16"
+    # Para los futuros procesadores AMD con arquitectura Zen 6
 
-        # Compilar el programa optimizado con AVX512 (nativo)
+    if grep -q "avx512fp16" /proc/cpuinfo; then
+        echo "AVX512FP16 support detected. Compiling with -mavx512fp16."
+
+        COMMON_FLAGS+=" -march=znver6 -mtune=znver6 -mavx512fp16"
+
+        # Compilar el programa optimizado con AVX512-FP16 (nativo para Zen 6)
         gcc-14 $COMMON_FLAGS pca_FP16.c -o pca_FP16_native-base $OPT_FLAGS $LINK_FLAGS
-        # Compilar el programa optimizado con AVX512 y precisión de 16 bits
-        gcc-14 $COMMON_FLAGS pca_FP16.c -o pca_FP16_avx512_precision -fexcess-precision=16 $OPT_FLAGS $LINK_FLAGS
-        # Compilar el programa con máxima optimización
+        # Compilar el programa optimizado con AVX512-FP16 y precisión estándar
+        gcc-14 $COMMON_FLAGS pca_FP16.c -o pca_FP16_avx512fp16_precision -fexcess-precision=16 $OPT_FLAGS $LINK_FLAGS
+        # Compilar el programa con máxima optimización para AVX512-FP16
         gcc-14 $COMMON_FLAGS pca_FP16.c -o pca_FP16_max_performance -fexcess-precision=16 -mfpmath=sse $OPT_FLAGS $LINK_FLAGS
 
-        # Eliminar los flags específicos de esta sección
-        COMMON_FLAGS="${COMMON_FLAGS/-mavx512fp16/}" 
+
+        # Eliminar los flags específicos de esta sección 
+        COMMON_FLAGS="${COMMON_FLAGS/-march=znver6/}"
+        COMMON_FLAGS="${COMMON_FLAGS/-mtune=znver6/}"
+        COMMON_FLAGS="${COMMON_FLAGS/-mavx512fp16/}"
         COMMON_FLAGS=$(echo $COMMON_FLAGS | tr -s ' ' | xargs)
 
     else
-        echo "AVX512FP16 not supported on this system. Skipping compilation with -mavx512fp16."
+        echo "AVX512 not supported on this system. Skipping compilation with AVX512 flags."
     fi
 
-else
+else 
     echo "SSE2 not supported on this system. Skipping compilation for programs with _Float16."
 fi
-
-# Compilación cruzada para ARM de 64 bits
 
 if $force_run; then
 
@@ -89,18 +92,15 @@ if $force_run; then
     # Compila para ARM de 64 bits, como distintivo el archivo tiene la extension .out
     aarch64-linux-gnu-gcc -Wall pca_FP32.c -o pca_FP32.out $LINK_FLAGS
 
-
     ### COMPILACION DEL PROGRAMA DE CON FLOAT DE 16 BITS QUE EMPLEA EL TIPO DE DATO _Float16
 
     # Compila para ARM de 64 bits, como distintivo el archivo tiene la extension .out
     aarch64-linux-gnu-gcc -Wall -fexcess-precision=16 pca_FP16.c -o pca_FP16.out $LINK_FLAGS
-    #gcc -o pca_program pca_program.c -larmpl_lp64 -larmpl_mp -lm
 
     ### COMPILACION DEL PROGRAMA DE CON FLOAT DE 16 BITS PARA ARQUITECTURA ARM (EMPLEA EL TIPO DE DATO __fp16)
 
     # Compila para ARM de 64 bits, como distintivo el archivo tiene la extension .out
     aarch64-linux-gnu-gcc -Wall pca_FP16_ARM.c -o pca_FP16_ARM.out $LINK_FLAGS
-
 
     ### COMPILACION DEL PROGRAMA DE CON FLOAT DE 16 BITS PARA ARQUITECTURA ARM (EMPLEA EL TIPO DE DATO __bf16)
 
@@ -108,3 +108,5 @@ if $force_run; then
     aarch64-linux-gnu-gcc -Wall pca_BF16.c -o pca_BF16.out $LINK_FLAGS
 
 fi
+
+exit 0
