5eeb33b59a488a6da7266b053723a796a701fdff
EloiBarconPineiro <eloi.b.pineiro@gmail.com>
2025-03-09 21:48:02 +0100

    Actualizacion script globales para mejorar gestion de directorios

--- Programas/compile_all.sh
+++ Programas/compile_all.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Inicializar variables
-force_run=false
 force_flag=""
 additional_flags=""
 
 # Verificar si el primer argumento es --force
 if [[ "$1" == "--force" ]]; then
-    force_run=true
+    force_flag="--force"
     shift
 else
     # Procesar argumentos de entrada
     for arg in "$@"; do
         if [[ "$arg" == "--force" ]]; then
-            force_run=true
+            force_flag="--force"
             set -- "${@/--force/}"
             break
         fi
@@ -44,79 +43,69 @@ ARCH=$(uname -m)
 
 # Obtener el proveedor del CPU
 VENDOR=$(grep -m 1 'vendor_id' /proc/cpuinfo | awk '{print $3}')
-# Asignar valor a force_flag basado en force_run
-if [ "$force_run" = true ]; then
-    force_flag="--force"
-fi
 
 # Dependiendo del proveedor del CPU, realizar diferentes acciones
 case "$VENDOR" in
     GenuineIntel)
-        CPU_VENDOR="intel"
+        CPU_VENDOR="Intel"
         ;;
     AuthenticAMD)
-        CPU_VENDOR="amd"
+        CPU_VENDOR="AMD"
         ;;
     HiSilicon)
-        CPU_VENDOR="huawei"
+        CPU_VENDOR="Huawei"
         ;;
     *)
         CPU_VENDOR="unknown"
         ;;
 esac
 
+# Directorios a procesar (se puede expandir fácilmente)
+DIRECTORIOS=("AXPY" "DCT" "DWT_1D" "PCA")
+
 # Dependiendo de la combinación de arquitectura y proveedor, realizar diferentes acciones
 case "$ARCH" in
     x86_64|amd64|x64)
-        case "$CPU_VENDOR" in
-            intel)
-                echo "Arquitectura: Intel de 64 bits"
-                # Instrucciones específicas para Intel de 64 bits
-                echo "Compilando AXPY"
-                ./AXPY/axpy_compile_Intel.sh $force_flag $additional_flags
-                echo "Compilando DCT"
-                ./DCT/dct_compile_Intel.sh $force_flag $additional_flags
-                echo "Compilando DWT_1D"
-                ./DWT_1D/dwt_1d_compile_Intel.sh $force_flag $additional_flags
-                echo "Compilando PCA"
-                ./PCA/pca_compile_Intel.sh $force_flag $additional_flags
-                ;;
-            amd)
-                echo "Arquitectura: AMD de 64 bits"
-                # Instrucciones específicas para AMD de 64 bits
-                echo "Compilando AXPY"
-                ./AXPY/axpy_compile_AMD.sh $force_flag
-                echo "Compilando DCT"
-                ./DCT/dct_compile_AMD.sh $force_flag
-                echo "Compilando DWT_1D"
-                ./DWT_1D/dwt_1d_compile_AMD.sh $force_flag
-                echo "Compilando PCA"
-                ./PCA/pca_compile_AMD.sh $force_flag
-                ;;
-            *)
-                echo "Vendor desconocido para x86_64"
-                ;;
-        esac
+
+        echo "Arquitectura: $CPU_VENDOR x86_64"
+        for DIR in "${DIRECTORIOS[@]}"; do
+
+            dir_lower=$(echo "$DIR" | tr '[:upper:]' '[:lower:]')  # Conversión a minúsculas
+            script="${DIR}/${dir_lower}_compile_${CPU_VENDOR}.sh"  # Formato: directorio/dir_compile_ARCH.sh
+            if [[ -f "$script" && -x "$script" ]]; then
+                echo "Compilando $DIR"
+                ./"$script" $force_flag $additional_flags
+            else
+                echo "Error: Script no encontrado o no ejecutable -> $script"
+            fi
+
+        done
+
         ;;
     aarch64|arm64)
-        echo "Arquitectura: ARM de 64 bits"
-        case "$CPU_VENDOR" in
-            huawei)
-                # Detecta por igual Huawei Ascend y HiSilicon Kunpeng
-                echo "Arquitectura: Huawei de 64 bits"
-                echo "No hay soporte oficial por el momento."
-                ;;
-            *)
-                echo "Compilando AXPY"
-                ./AXPY/axpy_compile_ARM.sh $force_flag
-                echo "Compilando DCT"
-                ./DCT/dct_compile_ARM.sh $force_flag
-                echo "Compilando DWT_1D"
-                ./DWT_1D/dwt_1d_compile_ARM.sh $force_flag
-                echo "Compilando PCA"
-                ./PCA/pca_compile_ARM.sh $force_flag
-                ;;
-        esac
+
+        echo "Arquitectura: $CPU_VENDOR ARM de 64 bits"
+
+        if [[ "$CPU_VENDOR" == "Huawei" ]]; then
+            echo "Arquitectura: Huawei de 64 bits"
+            echo "No hay soporte oficial por el momento."
+        else
+            CPU_VENDOR="ARM"
+        fi
+
+        for DIR in "${DIRECTORIOS[@]}"; do
+
+            dir_lower=$(echo "$DIR" | tr '[:upper:]' '[:lower:]')  # Conversión a minúsculas
+            script="${DIR}/${dir_lower}_compile_${CPU_VENDOR}.sh"  # Formato: directorio/dir_compile_ARCH.sh
+            if [[ -f "$script" && -x "$script" ]]; then
+                echo "Compilando $DIR"
+                ./"$script" $force_flag $additional_flags
+            else
+                echo "Error: Script no encontrado o no ejecutable -> $script"
+            fi
+
+        done
+
         ;;
     *)
         echo "Arquitectura desconocida o no soportada: $ARCH"
--- Programas/run_all.sh
+++ Programas/run_all.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Inicializar variables
-force_run=false
 force_flag=""
 
 # Verificar si el primer argumento es --force
 if [[ "$1" == "--force" ]]; then
-    force_run=true
+    force_flag="--force"
     shift
 else
     # Procesar argumentos de entrada
     for arg in "$@"; do
         if [[ "$arg" == "--force" ]]; then
-            force_run=true
+            force_flag="--force"
             set -- "${@/--force/}"
             break
         fi
@@ -56,79 +55,69 @@ ARCH=$(uname -m)
 
 # Obtener el proveedor del CPU
 VENDOR=$(grep -m 1 'vendor_id' /proc/cpuinfo | awk '{print $3}')
-# Asignar valor a force_flag basado en force_run
-if [ "$force_run" = true ]; then
-    force_flag="--force"
-fi
 
 # Dependiendo del proveedor del CPU, realizar diferentes acciones
 case "$VENDOR" in
     GenuineIntel)
-        CPU_VENDOR="intel"
+        CPU_VENDOR="Intel"
         ;;
     AuthenticAMD)
-        CPU_VENDOR="amd"
+        CPU_VENDOR="AMD"
         ;;
     HiSilicon)
-        CPU_VENDOR="huawei"
+        CPU_VENDOR="Huawei"
         ;;
     *)
         CPU_VENDOR="unknown"
         ;;
 esac
 
+# Directorios a procesar (se puede expandir fácilmente)
+DIRECTORIOS=("AXPY" "DCT" "DWT_1D" "PCA")
+
 # Dependiendo de la combinación de arquitectura y proveedor, realizar diferentes acciones
 case "$ARCH" in
     x86_64|amd64|x64)
-        case "$CPU_VENDOR" in
-            intel)
-                echo "Arquitectura: Intel de 64 bits"
-                # Instrucciones específicas para Intel de 64 bits
-                echo "Ejecutando AXPY"
-                ./AXPY/axpy_run_Intel.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando DCT"
-                ./DCT/dct_run_Intel.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando DWT_1D"
-                ./DWT_1D/dwt_1d_run_Intel.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando PCA"
-                ./PCA/pca_run_Intel.sh $tamanhoN $seed $force_flag
-                ;;
-            amd)
-                echo "Arquitectura: AMD de 64 bits"
-                # Instrucciones específicas para AMD de 64 bits
-                echo "Ejecutando AXPY"
-                ./AXPY/axpy_run_AMD.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando DCT"
-                ./DCT/dct_run_AMD.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando DWT_1D"
-                ./DWT_1D/dwt_1d_run_AMD.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando PCA"
-                ./PCA/pca_run_AMD.sh $tamanhoN $seed $force_flag
-                ;;
-            *)
-                echo "Vendor desconocido para x86_64"
-                ;;
-        esac
+
+        echo "Arquitectura: $CPU_VENDOR x86_64"
+        for DIR in "${DIRECTORIOS[@]}"; do
+
+            dir_lower=$(echo "$DIR" | tr '[:upper:]' '[:lower:]')  # Conversión a minúsculas
+            script="${DIR}/${dir_lower}_run_${CPU_VENDOR}.sh"  # Formato: directorio/dir_run_ARCH.sh
+            if [[ -f "$script" && -x "$script" ]]; then
+                echo "Ejecutando $DIR"
+                ./"$script" $tamanhoN $seed $force_flag
+            else
+                echo "Error: Script no encontrado o no ejecutable -> $script"
+            fi
+
+        done
+        
         ;;
     aarch64|arm64)
-        echo "Arquitectura: ARM de 64 bits"
-        case "$CPU_VENDOR" in
-            huawei)
-                # Detecta por igual Huawei Ascend y HiSilicon Kunpeng
-                echo "Arquitectura: Huawei de 64 bits"
-                echo "No hay soporte oficial por el momento."
-                ;;
-            *)
-                echo "Ejecutando AXPY"
-                ./AXPY/axpy_run_ARM.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando DCT"
-                ./DCT/dct_run_ARM.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando DWT_1D"
-                ./DWT_1D/dwt_1d_run_ARM.sh $tamanhoN $seed $force_flag
-                echo "Ejecutando PCA"
-                ./PCA/pca_run_ARM.sh $tamanhoN $seed $force_flag
-                ;;
-        esac
+
+        echo "Arquitectura: $CPU_VENDOR ARM de 64 bits"
+
+        if [[ "$CPU_VENDOR" == "Huawei" ]]; then
+            echo "Arquitectura: Huawei de 64 bits"
+            echo "No hay soporte oficial por el momento."
+        else
+            CPU_VENDOR="ARM"
+        fi
+
+        for DIR in "${DIRECTORIOS[@]}"; do
+
+            dir_lower=$(echo "$DIR" | tr '[:upper:]' '[:lower:]')  # Conversión a minúsculas
+            script="${DIR}/${dir_lower}_run_${CPU_VENDOR}.sh"  # Formato: directorio/dir_run_ARCH.sh
+            if [[ -f "$script" && -x "$script" ]]; then
+                echo "Ejecutando $DIR"
+                ./"$script" $tamanhoN $seed $force_flag
+            else
+                echo "Error: Script no encontrado o no ejecutable -> $script"
+            fi
+
+        done
+
         ;;
     *)
         echo "Arquitectura desconocida o no soportada: $ARCH"
